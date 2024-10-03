b9a21f37c4813e845c0c81f870c7072904dd1c27
andy <andrew.muro@generac.com>
2024-10-03 12:07:13 -0700

    init

---
+++ index.js
@@ -0,0 +1,122 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+const URL = require('node:url')
+const path = require('node:path')
+
+const _ = require('lodash')
+const pug = require('pug')
+const {parse} = require('csv-parse/sync')
+const {DateTime} = require('luxon')
+const randomColor = require('randomcolor')
+const ics = require('ics')
+const axios = require('axios')
+const Promise = require('bluebird')
+const xml2js = require('xml2js')
+
+const PER_PAGE = 8
+const DRIVE_ID_REGEX = /d\/(.+)\/view/
+const ICON_PATH = path.join(__dirname, 'icons')
+const MAX_TITLE_SIZE = 4.7
+const MIN_TITLE_SIZE = 2.5
+
+async function render(argv){
+  let tpl = pug.compileFile('template.pug', {pretty: true})
+  let rows = parse(fs.readFileSync(argv.csv), {columns: true})
+
+  let events = await Promise.mapSeries(rows, async row => {
+    let date = DateTime.fromISO(row.DATE)
+    let start = DateTime.fromISO(row['START']).toFormat('h:mma')
+    let icon
+
+    if (fs.accessSync(`${ICON_PATH}/${row.ICON}.svg`, fs.constants.R_OK) == null){
+      icon = fs.readFileSync(`${ICON_PATH}/${row.ICON}.svg`)
+
+      let parser = new xml2js.Parser()
+      let svgData = await parser.parseStringPromise(icon)
+      delete svgData.svg['$'].fill
+      delete svgData.svg['$'].height
+      delete svgData.svg['$'].width
+      delete svgData.svg['$'].style
+      delete svgData.svg.style
+      _.set(svgData, 'svg.path[0].$.fill', null)
+
+      let builder = new xml2js.Builder()
+      icon = builder.buildObject(svgData)
+    }
+    else {
+      console.warn(`Icon not found: ${row.ICON}`)
+    }
+
+    let titleSize = -0.1671533 * row.TITLE.length + 8.5
+    if (titleSize > MAX_TITLE_SIZE){
+      titleSize = MAX_TITLE_SIZE
+    }
+    if (titleSize < MIN_TITLE_SIZE){
+      titleSize = MIN_TITLE_SIZE
+    }
+
+    return {
+      date,
+      dow: date.toFormat('ccc'),
+      day: date.toFormat('d'),
+      type: row.TYPE,
+      name: row.TITLE,
+      icon,
+      start: row.END ? `${start}-`: start,
+      end: row.END ? DateTime.fromISO(row.END).toFormat('h:mma'): '',
+      color: row.HEX,
+      size: `${titleSize}em`
+    }
+
+//    let driveId = row.ICON.match(DRIVE_ID_REGEX)[1]
+//    let resp = await axios.get('https://drive.google.com/uc', {
+//      params: {
+//        export: 'download',
+//        id: driveId,
+//        confirm: 'yes'
+//      }
+//    })
+  })
+
+  let pages = _.chunk(events, PER_PAGE)
+
+  let info = {
+    title: events[0].date.toFormat('LLLL yyyy')
+  }
+
+  console.log(tpl({events: pages[argv.page - 1], info}))
+}
+
+function generateIcs(argv){
+  let rows = parse(fs.readFileSync(argv.csv), {columns: true})
+
+  let events = rows.map(row => {
+    let date = DateTime.fromISO(row.DATE)
+    let start = DateTime.fromISO(row['START'])
+    let end = row.END ? DateTime.fromISO(row['END']): start.plus({hours: 2})
+    let diff = end.diff(start, ['hours', 'minutes'])
+
+    return {
+      title:`[${row.TYPE}] ${row.NAME}`,
+      start: [date.year, date.month, date.day, start.hour, start.minute],
+      duration: {hours: diff.hours, minutes: diff.minutes},
+      description: row.DESC,
+      location: row.LOCATION
+    }
+  })
+
+  let {error, value} = ics.createEvents(events)
+  console.log(value)
+}
+
+async function run(){
+  await require('yargs')
+    .command('render <csv>', '', {}, render)
+    .command('ics <csv>', '', {}, generateIcs)
+    .option('page', {type: Number, default: 1})
+    .option('no-title', {type: Boolean})
+    .parse()
+}
+
+run()
